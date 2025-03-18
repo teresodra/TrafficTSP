510ddc13ba931a796f60445e895304a2c2ecb2f1
Tereso del Rio <delriot@coventry.ac.uk>
2025-03-18 18:38:33 +0100

    ⚰️ (rem): Remove dead code to discretise graph

--- trafficTSP/Approaches/DiscreteWithGraph/discretise_graph.py
+++ trafficTSP/Approaches/DiscreteWithGraph/discretise_graph.py
@@ -31,35 +31,3 @@ def discretise_and_approximate_graph(graph: dict, bins: int) -> dict:
                     # Add the weight to the DataFrame
                     df.loc[(i, j, step), 'value'] = round(weight/timestep)
     return df
-
-
-def discretise_graph(graph: dict, bins: int = 10) -> dict:
-    """
-    Discretises the weight of the graph from a continious function
-    to a finite set.
-    """
-    n_nodes = graph['n_nodes']
-    time_range = graph['time_range']
-    discrete_timestamps = [
-        time_range[0] + i*(time_range[1] - time_range[0])/bins
-        for i in range(bins+1)]
-    discrete_timestamps = np.linspace(time_range[0], time_range[1], num=bins)
-
-    # Create the MultiIndex for the DataFrame
-    multi_index = pd.MultiIndex.from_product(
-        [range(n_nodes), range(n_nodes), discrete_timestamps],
-        names=['start_node', 'final_node', 'time']
-    )
-
-    # Initialize the DataFrame
-    df = pd.DataFrame(index=multi_index, columns=['cost'])
-    # Iterate over all the edges
-    for i in range(n_nodes):
-        for j in range(n_nodes):
-            if i != j:
-                for time in discrete_timestamps:
-                    # Calculate the weight at time t
-                    weight = graph[(i, j)](time)
-                    # Add the weight to the DataFrame
-                    df.loc[(i, j, time), 'value'] = weight
-    return df
