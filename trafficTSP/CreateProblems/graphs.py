98f9559b01fc1ebfc19751e5319c7f7d2dbd7962
Tereso del Rio <delriot@coventry.ac.uk>
2025-03-18 11:59:44 +0100

    ✨ (add): Saved time range used in graph

--- trafficTSP/CreateProblems/graphs.py
+++ trafficTSP/CreateProblems/graphs.py
@@ -5,7 +5,10 @@ from trafficTSP.CreateProblems.weight_functions import (
 )
 
 
-def create_graph(n_nodes: int, max_distance: float = 10) -> dict:
+def create_graph(n_nodes: int,
+                 max_distance: float = 10,
+                 time_range: tuple[float, float] = (0, 480)
+                 ) -> dict:
     """
     Returns a graph saved in a dictionary
     with n_nodes and weight functions for the edges.
@@ -17,11 +20,16 @@ def create_graph(n_nodes: int, max_distance: float = 10) -> dict:
              for _ in range(n_nodes)]
 
     # Create a graph with random weight functions
-    graph = {'n_nodes': n_nodes}
+    graph = {
+        'n_nodes': n_nodes,
+        'time_range': time_range
+        }
     for i in range(n_nodes):
         for j in range(n_nodes):
             if i != j:
                 graph[(i, j)] = create_random_weight_function(
-                    nodes[i], nodes[j]
+                    nodes[i],
+                    nodes[j],
+                    mean_range=time_range
                 )
     return graph
