66f423083fc6e5c99c712e7021491a79c8e9ee58
Tereso del Rio <delriot@coventry.ac.uk>
2025-03-24 00:28:54 +0100

    🎨 (defaults): Updating defaults in main.py

--- main.py
+++ main.py
@@ -1,37 +1,37 @@
 from trafficTSP.Compare.compare_strategies import compare_strategies
 from trafficTSP.Approaches.Greedy.greedy_strategy import GreedyStrategy
-from trafficTSP.Approaches.Random.random_strategy import RandomStrategy
-from trafficTSP.Approaches.DiscreteWithGraph.discrete_strategy import (
+# from trafficTSP.Approaches.Random.random_strategy import RandomStrategy
+from trafficTSP.Approaches.Discrete.discrete_strategy import (
     DiscreteStrategy
 )
 import argparse
 import random
-import time
 
 
 def main():
     # Set up argument parsing
     parser = argparse.ArgumentParser()
     parser.add_argument('--nodes', type=int,
-                        help='Number of nodes')
+                        help='Number of nodes',
+                        default=9)
+    parser.add_argument('--n_repetitions', type=int,
+                        help='Number of repetitions',
+                        default=20)
     args = parser.parse_args()
 
     # Compare strategies
     compare_strategies(
         strategies={
             "Greedy": GreedyStrategy,
-            "Random": RandomStrategy,
+            # "Random": RandomStrategy,
             "Discrete": DiscreteStrategy
         },
         max_nodes=args.nodes,
-        n_repetitions=2
+        n_repetitions=args.n_repetitions
     )
 
 
 if __name__ == "__main__":
     # Fix the seed for reproducibility
     random.seed(0)
-    start_time = time.time()
     main()
-    end_time = time.time()
-    print(f"Total time: {end_time - start_time} seconds.")
